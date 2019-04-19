09ad3307a998f4b9d536b8e90e8e1592ed1e94c4
horodnicm7 <horodnicm7@gmail.com>
2019-04-19 22:23:15 +0300

    Removed the C++ version

--- decryptor/constants.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define CHUNK 2000
-
-char tags[16][10] = {"<div", "<link", "<body", "<head", "<h1", "<h2", "<h3",
-        "<sup", "<span", "<a", "<p", "<table", "<b", "<ul", "<li", "<style"
-};
-const int tags_len = 16;
--- decryptor/utils.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-int get_file_size(const char *filename);
-void fill_longest_prefix_suffix(char *pattern, int M, int *lps);
-long get_first_occurence(char *source, char *what);
-long get_first_occurence(char *source, char what);
